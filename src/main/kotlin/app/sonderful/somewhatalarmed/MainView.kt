f2d57f6fb19ce172430a0113affa706b4859cb27
Stuart Gilbert <stuart.gilbert@gmail.com>
2020-09-21 21:49:24 -0700

    Minor layout improvements to make my life easier during development
    Set some default sizes for the UID and Name columns. Moved the `load` button and label around slightly.

--- src/main/kotlin/app/sonderful/somewhatalarmed/MainView.kt
+++ src/main/kotlin/app/sonderful/somewhatalarmed/MainView.kt
@@ -2,6 +2,7 @@ package app.sonderful.somewhatalarmed
 
 import app.sonderful.somewhatalarmed.types.Package
 import javafx.beans.binding.Bindings
+import javafx.geometry.Pos
 import javafx.scene.control.SelectionMode
 import javafx.stage.FileChooser
 import tornadofx.*
@@ -15,29 +16,36 @@ class MainView : View("Somewhat Alarmed") {
             spacing = 5.px
             padding = box(20.px)
         }
-        label("Load the output of `adb shell dumpsys alarm`")
-        button("Choose Input File") {
-            action {
-                val filename = chooseFile(
-                    "Select output of dumpsys alarm",
-                    arrayOf(
-                        FileChooser.ExtensionFilter("Text files", "*.txt"),
-                        FileChooser.ExtensionFilter("All files", "*"),
-                    ),
-                    null,
-                    null,
-                    FileChooserMode.Single
-                )
-
-                viewModel.readAndParseFile(filename[0])
+        hbox {
+            style {
+                spacing = 5.px
+                padding = box(20.px)
+                alignment = Pos.CENTER_LEFT
             }
+            button("Choose Input File") {
+                action {
+                    val filename = chooseFile(
+                        "Select output of dumpsys alarm",
+                        arrayOf(
+                            FileChooser.ExtensionFilter("Text files", "*.txt"),
+                            FileChooser.ExtensionFilter("All files", "*"),
+                        ),
+                        null,
+                        null,
+                        FileChooserMode.Single
+                    )
+
+                    viewModel.readAndParseFile(filename[0])
+                }
+            }
+            label("Load the output of dumpsys alarm")
         }
         label("Detected Packages")
         val tv = tableview(viewModel.packages) {
             tooltip("Choose a package to load alarm details")
             selectionModel.selectionMode = SelectionMode.SINGLE
-            readonlyColumn("UID", Package::uid)
-            readonlyColumn("Name", Package::name)
+            readonlyColumn("UID", Package::uid).prefWidth(75)
+            readonlyColumn("Name", Package::name).prefWidth(350)
             onUserSelect {
                 viewModel.loadDetailsForPackage(it)
             }
