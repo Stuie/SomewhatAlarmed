fb44b46b235c3dd267f53192380ee8ef84fd8ad2
Stuart Gilbert <stuart.gilbert@gmail.com>
2020-09-23 14:41:33 -0700

    Added some details to the alarm detail view
    I added in the absolute basics for the application to be useful for the scenario I wanted it. There's a lot more than can be added, and it can certainly be improved upon.

--- src/main/kotlin/app/sonderful/somewhatalarmed/presentation/detail/AlarmDetailView.kt
+++ src/main/kotlin/app/sonderful/somewhatalarmed/presentation/detail/AlarmDetailView.kt
@@ -16,6 +16,7 @@
 package app.sonderful.somewhatalarmed.presentation.detail
 
 import app.sonderful.somewhatalarmed.types.Alarm
+import javafx.scene.text.TextAlignment
 import tornadofx.View
 import tornadofx.*
 
@@ -29,5 +30,89 @@ class AlarmDetailView : View("Alarm Details") {
             padding = box(20.px)
         }
         label("Alarm: ${currentAlarm.tag}")
+        hbox {
+            style {
+                spacing = 5.px
+            }
+            label("Alarm Type") {
+                prefWidth = 200.0
+                textAlignment = TextAlignment.LEFT
+            }
+            label("${currentAlarm.type}") {
+                tooltip("`type` refers to the type of alarm\n\nRTC = Real Time Clock\nELAPSED = Time since device reboot\nRTC_WAKEUP and ELAPSED_WAKEUP are the same as above, but specifically intended to wake up the device.")
+            }
+        }
+        hbox {
+            style {
+                spacing = 5.px
+            }
+            label("When") {
+                prefWidth = 200.0
+                textAlignment = TextAlignment.LEFT
+            }
+            label("${currentAlarm.timeWhen}") {
+                tooltip("`when` refers to the time when this alarm should be triggered.\nThis may be inaccurate if your alarm is batched with other alarms, as in that case the latest `when` in the batched alarms will be used.")
+            }
+        }
+        hbox {
+            style {
+                spacing = 5.px
+            }
+            label("When Elapsed") {
+                prefWidth = 200.0
+                textAlignment = TextAlignment.LEFT
+            }
+            label("${currentAlarm.whenElapsed}") {
+                tooltip("`whenElapsed` refers to the approximate number of milliseconds since device boot at which the alarm will be triggered.")
+            }
+        }
+        hbox {
+            style {
+                spacing = 5.px
+            }
+            label("Repeat Interval") {
+                prefWidth = 200.0
+                textAlignment = TextAlignment.LEFT
+            }
+            label("${currentAlarm.repeatInterval}") {
+                tooltip("`repeatInterval` represents the numbber of milliseconds between successive triggers of the alarm. 0 means the alarm does not repeat.")
+            }
+        }
+        hbox {
+            style {
+                spacing = 5.px
+            }
+            label("Count") {
+                prefWidth = 200.0
+                textAlignment = TextAlignment.LEFT
+            }
+            label("${currentAlarm.count}") {
+                tooltip("`count` refers to the number of times the alarm should have been triggered, but was not.`")
+            }
+        }
+        hbox {
+            style {
+                spacing = 5.px
+            }
+            label("Operation") {
+                prefWidth = 200.0
+                textAlignment = TextAlignment.LEFT
+            }
+            label("${currentAlarm.operation}") {
+                tooltip("`operation` refers to what will happen when the alarm is triggered, e.g. a PendingIntent that will be triggered.\nThis may be null.")
+            }
+        }
+        hbox {
+            style {
+                spacing = 5.px
+            }
+            label("Listener") {
+                prefWidth = 200.0
+                textAlignment = TextAlignment.LEFT
+            }
+            label("${currentAlarm.listener}") {
+                tooltip("`listener` refers to what action will be taken when the alarm is triggered.\nThis may be null. I've only seen it set when `operation` is null.")
+            }
+        }
     }
 }
